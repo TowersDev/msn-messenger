a54ef03876c74b694522c7bdfb1553dabeb8d0f8
Jesús Torres <torresjmtd@gmail.com>
2022-11-12 11:33:28 +0100

    refactor

--- src/app/pages/chat/chat.page.ts
+++ src/app/pages/chat/chat.page.ts
@@ -1,47 +1,64 @@
-import { Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { IonContent, IonList } from '@ionic/angular';
+import { IonContent, IonFooter, IonList } from '@ionic/angular';
 
 @Component({
   selector: 'app-chat',
   templateUrl: 'chat.page.html',
-  styleUrls: ['chat.page.scss']
+  styleUrls: ['chat.page.scss'],
 })
-export class ChatPage implements OnInit{
-  @ViewChild('pru') scroll: any;
+export class ChatPage implements OnInit {
+  @ViewChild(IonContent, { static: false }) content: IonContent;
+
   name: string;
   message: string;
   listChat: any;
   user: any;
 
-  constructor(private readonly firestore: AngularFirestore, public ngFireAuth: AngularFireAuth) {}
+  data: any = {
+    sender: '',
+    message: '',
+    date: new Date(),
+  };
+
+  constructor(
+    private readonly firestore: AngularFirestore,
+    public ngFireAuth: AngularFireAuth
+  ) {}
 
   ngOnInit() {
     this.ngFireAuth.authState.subscribe((user) => {
       this.user = user;
-    });
-
-    this.firestore.collection('messages').valueChanges().subscribe((message) => {
-      this.listChat = message?.sort((a: any,b: any) => a.date - b.date);
+      this.loadChat();
     });
   }
 
-  prueba() {
-    const prueba = {
+  loadChat() {
+    this.firestore
+      .collection('messages')
+      .valueChanges()
+      .subscribe((message) => {
+        this.listChat = message?.sort((a: any, b: any) => a.date - b.date);
+      });
+  }
+
+  sendMessage() {
+    this.data = {
       sender: this.user.email,
       message: this.message,
       date: new Date(),
-    };
-    this.firestore.collection('messages').add(prueba);
-    this.message = '';
-
-    this.scrollToBottom();
-
-
+    }; // ARREGLAR
+    this.firestore
+      .collection('messages')
+      .add(this.data)
+      .then((res) => {
+        this.message = '';
+        this.scrollToBottom();
+      });
   }
 
   scrollToBottom() {
-    this.scroll.nativeElement.scrollTop = this.scroll.nativeElement.scrollHeight;
+    this.content.scrollToBottom(300);
   }
 }
