c8f90f3a56fde3584a6c85f8626aa90c98856f73
Jesús Torres <torresjmtd@gmail.com>
2022-11-26 01:23:02 +0100

    add settings and new icons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,33 +4,49 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'login',
-    loadChildren: () => import('./pages/account/login/login.module').then(m => m.LoginPageModule),
+    loadChildren: () =>
+      import('./pages/account/login/login.module').then(
+        (m) => m.LoginPageModule
+      ),
     // canLoad: [IntroGuard, AutoLoginGuard]
   },
   {
     path: 'register',
-    loadChildren: () => import('./pages/account/register/register.module').then(m => m.RegisterPageModule),
+    loadChildren: () =>
+      import('./pages/account/register/register.module').then(
+        (m) => m.RegisterPageModule
+      ),
     // canLoad: [IntroGuard, AutoLoginGuard]
   },
   {
     path: 'intro',
-    loadChildren: () => import('./pages/intro/intro.module').then( m => m.IntroPageModule)
+    loadChildren: () =>
+      import('./pages/intro/intro.module').then((m) => m.IntroPageModule),
   },
   {
     path: 'tabs',
-    loadChildren: () => import('./components/tabs/tabs.module').then(m => m.TabsPageModule),
+    loadChildren: () =>
+      import('./components/tabs/tabs.module').then((m) => m.TabsPageModule),
+    // canLoad: [AuthGuard]
+  },
+  {
+    path: 'settings',
+    loadChildren: () =>
+      import('./pages/account/settings/settings.module').then(
+        (m) => m.SettingsPageModule
+      ),
     // canLoad: [AuthGuard]
   },
   {
     path: '',
     redirectTo: '/login',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
 ];
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/pages/account/account.page.ts
+++ src/app/pages/account/account.page.ts
@@ -1,17 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AuthenticationService } from '../../services/authentication.service';
-import { ActionSheetController , Platform, ToastController} from '@ionic/angular';
+import {
+  ActionSheetController,
+  Platform,
+  ToastController,
+} from '@ionic/angular';
 
 // import { ImagePicker } from '@awesome-cordova-plugins/image-picker/ngx';
 import { Camera, CameraOptions } from '@awesome-cordova-plugins/camera/ngx';
-import { ImagePicker, ImagePickerOptions } from '@awesome-cordova-plugins/image-picker/ngx';
+import {
+  ImagePicker,
+  ImagePickerOptions,
+} from '@awesome-cordova-plugins/image-picker/ngx';
 import { WebView } from '@ionic-native/ionic-webview/ngx';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { finalize } from 'rxjs/operators';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-
-
 
 // import { File } from '@awesome-cordova-plugins/File/ngx';
 // import { Crop } from '@ionic-native/crop/ngx';
@@ -36,9 +41,9 @@ export class AccountPage implements OnInit {
     private webView: WebView,
     private storage: AngularFireStorage,
     private afs: AngularFirestore
-    // private file: File,
-    // public crop: Crop
-  ) { }
+  ) // private file: File,
+  // public crop: Crop
+  {}
 
   ngOnInit() {
     this.ngFireAuth.authState.subscribe((user) => {
@@ -61,27 +66,32 @@ export class AccountPage implements OnInit {
 
     const ref = this.storage.ref(`avatar/${this.user.uid}`);
     return ref.put(blob);
-  };
+  }
 
   actualizarAvatar(image: any) {
-
-    this.storage.ref(`avatar/${this.user.uid}`).getDownloadURL().toPromise()
-    .then(async (res) => {
-      const update = {
-        photoURL: res,
-      };
-      return (await this.ngFireAuth.currentUser).updateProfile(update);
-    })
-    .catch(() => this.presentToast('Error al actualizar el avatar.'));
-    this.afs.collection('users').doc(this.user.uid).update({
-      avatar: image, // Photo URL from Firebase Storage will be saved in here.
-    }).then(() => {
-      console.log('success');
-    })
-    .catch(error => {
-      console.log('errors');
-    });
-
+    this.storage
+      .ref(`avatar/${this.user.uid}`)
+      .getDownloadURL()
+      .toPromise()
+      .then(async (res) => {
+        const update = {
+          photoURL: res,
+        };
+        return (await this.ngFireAuth.currentUser).updateProfile(update);
+      })
+      .catch(() => this.presentToast('Error al actualizar el avatar.'));
+    this.afs
+      .collection('users')
+      .doc(this.user.uid)
+      .update({
+        avatar: image, // Photo URL from Firebase Storage will be saved in here.
+      })
+      .then(() => {
+        console.log('success');
+      })
+      .catch((error) => {
+        console.log('errors');
+      });
   }
 
   elegirDesdeCamara(sourceType) {
@@ -91,29 +101,32 @@ export class AccountPage implements OnInit {
       destinationType: this.camera.DestinationType.FILE_URI,
       encodingType: this.camera.EncodingType.JPEG,
       sourceType: this.camera.PictureSourceType.CAMERA,
-      correctOrientation: true
+      correctOrientation: true,
     };
 
-    this.camera.getPicture(options).then((image) => {
-        this.imageUrl =   this.webView.convertFileSrc(image);
+    this.camera.getPicture(options).then(
+      (image) => {
+        this.imageUrl = this.webView.convertFileSrc(image);
         this.uploadImage(this.imageUrl)
-        .then(() => {
-          this.actualizarAvatar(this.imageUrl);
-        })
-        .catch((error) => {
-          this.presentToast(error);
-        });
-    }, error => {
-      console.log('No se ha realizado ninguna foto');
-    });
+          .then(() => {
+            this.actualizarAvatar(this.imageUrl);
+          })
+          .catch((error) => {
+            this.presentToast(error);
+          });
+      },
+      (error) => {
+        console.log('No se ha realizado ninguna foto');
+      }
+    );
   }
 
-  elegirDesdeGallery(){
-    this.imagePicker.hasReadPermission().then(res => {
+  elegirDesdeGallery() {
+    this.imagePicker.hasReadPermission().then((res) => {
       console.log('permissiopns status = ', res);
       if (res === false) {
-        this.imagePicker.requestReadPermission().then(res1 => {
-          console.log('requested permissions status = ',res1);
+        this.imagePicker.requestReadPermission().then((res1) => {
+          console.log('requested permissions status = ', res1);
         });
       } else {
         const options: ImagePickerOptions = {
@@ -122,25 +135,27 @@ export class AccountPage implements OnInit {
           width: 500,
           height: 500,
         };
-        this.imagePicker.getPictures(options).then((result) => {
-          console.log('selected photos = ', result);
-          if(result.length > 0) {
-
-            // eslint-disable-next-line @typescript-eslint/prefer-for-of
-            for(let i=0; i<result.length; i++){
-              this.url =  this.webView.convertFileSrc(result[i]);
+        this.imagePicker.getPictures(options).then(
+          (result) => {
+            console.log('selected photos = ', result);
+            if (result.length > 0) {
+              // eslint-disable-next-line @typescript-eslint/prefer-for-of
+              for (let i = 0; i < result.length; i++) {
+                this.url = this.webView.convertFileSrc(result[i]);
+              }
+              this.uploadImage(this.url)
+                .then(() => {
+                  this.actualizarAvatar(this.imageUrl);
+                })
+                .catch((error) => {
+                  this.presentToast(error);
+                });
             }
-            this.uploadImage(this.url)
-            .then(() => {
-              this.actualizarAvatar(this.imageUrl);
-            })
-            .catch((error) => {
-              this.presentToast(error);
-            });
+          },
+          (error) => {
+            console.log('No se ha seleccionado ninguna imagen');
           }
-        }, error => {
-          console.log('No se ha seleccionado ninguna imagen');
-        });
+        );
       }
     });
   }
@@ -154,7 +169,7 @@ export class AccountPage implements OnInit {
           handler: () => {
             this.elegirDesdeGallery();
             console.log('select image from gallery');
-          }
+          },
         },
         {
           text: 'selet camera',
@@ -162,13 +177,14 @@ export class AccountPage implements OnInit {
             console.log('select camera');
             this.elegirDesdeCamara(this.camera.PictureSourceType.CAMERA);
             console.log(this.platform.is('cordova'));
-          }
-        }
-      ]
+          },
+        },
+      ],
     });
     await actionSheet.present();
   }
+
+  prueba() {
+    console.log('entra');
+  }
 }
-
-
-
